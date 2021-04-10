5eabf1afa51f7701ab26b4aa2c66729abb538c9f
YaponEran <erannodepp9.4@gmail.com>
2021-04-10 19:26:11 +0600

    question and answer assosiation with rspec test

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
 
+  has_many :answers, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true
   validates :tag, presence: true
