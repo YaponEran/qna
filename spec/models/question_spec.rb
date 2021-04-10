790466fd6936c4a1e08793c4792dc653f186e65f
YaponEran <erannodepp9.4@gmail.com>
2021-04-10 19:44:53 +0600

    question validate inclussin with rspec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :tag }
 
   it { should have_many(:answers) }
+
+  it { should validate_inclusion_of(:tag).
+       in_array(['art', 'history', 'sport', 'code', 'game'])
+     }
 end
