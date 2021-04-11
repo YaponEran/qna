3560746880c260e1b64a288a092ffcb5b796dc10
YaponEran <erannodepp9.4@gmail.com>
2021-04-11 19:51:41 +0600

    deleted tag column and dependent with rspec

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,7 @@
 class Question < ApplicationRecord
 
-  TAG_NAME = %w[art history sport code game]
-
   has_many :answers, dependent: :destroy
 
-  validates :title, :body, :tag, presence: true
-  validates :tag, inclusion: { in: TAG_NAME }
+  validates :title, :body, presence: true
+
 end
