07998c9a482d82e51917df13acaf00951780899a
YaponEran <erannodepp9.4@gmail.com>
2021-04-11 19:51:41 +0600

    deleted tag column and dependent with rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_121307) do
+ActiveRecord::Schema.define(version: 2021_04_11_134837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,6 @@ ActiveRecord::Schema.define(version: 2021_04_10_121307) do
   create_table "questions", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
-    t.string "tag", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should validate_presence_of :tag }
 
-  it { should have_many(:answers) }
-
-  it { should validate_inclusion_of(:tag).
-       in_array(['art', 'history', 'sport', 'code', 'game'])
-     }
+  it { should have_many(:answers).dependent(:destroy) }
 end
