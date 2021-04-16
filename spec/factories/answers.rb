a357b68c707b86748f39d31db4151d4afb054d08
YaponEran <erannodepp9.4@gmail.com>
2021-04-16 10:17:37 +0600

    Answer controller with rspec and all tests shows green

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :answer do
-    
+    body { "MyText" }
+    point { 1 }
+    accept { true }
+    question { association :question }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
