2b2c1313408c35756716688448a9b20dcc8c55da
YaponEran <erannodepp9.4@gmail.com>
2021-04-21 08:13:41 +0600

    Unregistered user tries sign in

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -18,5 +18,10 @@ feature 'User can sign in', %q{
   end
 
   scenario 'Unregistered user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password'
   end
 end
