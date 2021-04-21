ba49e56e5943be507d096c4c611ee8a537d986c8
YaponEran <erannodepp9.4@gmail.com>
2021-04-21 08:18:18 +0600

    resolved conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
   devise_for :users
   root to: "questions#index"
-=======
->>>>>>> 13c0805273e520b39b3768c822b825e9c97d5578
-  
+
   resources :questions do
     resources :answers, shallow: true
   end
