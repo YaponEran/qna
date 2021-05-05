bfce4d8656b15ae92686998df996c452c1f1be86
YaponEran <erannodepp9.4@gmail.com>
2021-05-05 16:38:48 +0600

    answr with edit update rspec js added

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -87,4 +87,63 @@ RSpec.describe AnswersController, type: :controller do
     end
   end
 
+  describe 'PATCH #update' do
+
+    let!(:answer) { create(:answer, question: question, user: user) }
+
+    context 'Authorized user edits his answer with valid attributes' do
+      before { login(user) }
+
+      it 'changes answer attributes' do
+        patch :update, params: {id: answer, answer: {body: 'new body'}}, format: :js
+        answer.reload
+        expect(answer.body).to eq 'MyText'
+      end
+
+      it 'renders update view' do
+        patch :update, params: {id: answer, answer: {body: 'new body'}}, format: :js
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'Authorized user edits his answer with invalid attributes' do
+      before { login(user) }
+
+      it 'does not change answer attributes' do
+        expect do
+          patch :update, params: {id: answer, answer: attributes_for(:answer, :invalid)}, format: :js
+        end.to_not change(answer, :body)
+      end
+
+      it 'renders update view' do
+        patch :update, params: {id: answer, answer: attributes_for(:answer, :invalid)}, format: :js
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'User is not author' do
+      let(:other_user) { create(:user) }
+      before { login(:other_user) }
+
+      it "tries to edit others's user answer" do
+        patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
+        answer.reload
+        expect(answer.body).to_not eq 'new body'
+      end
+    end
+
+    context 'Unauthorized user' do
+      it 'tries to edit answer' do
+        patch :update, params: {id: answer, answer: {body: 'new body'}}, format: :js
+        answer.reload
+        expect(answer.body).to_not eq 'new body'
+      end
+
+      it 'needs to login in order to proceed' do
+        patch :update, params: { id: answer, answer: { body: 'new body' } }, format: :js
+        expect(response.body).to have_content 'You need to sign in or sign up before continuing.'
+      end
+    end
+  end
+
 end
