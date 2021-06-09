11690f3f4960017f6bf82989708f5cf500d9e0fe
YaponEran <erannodepp9.4@gmail.com>
2021-06-09 17:45:55 +0600

    doorkeeper.rb changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'oauth_callbacks' }
   root to: "questions#index"
 
   resources :questions do
